aafd01becc8badd59ca7ed0ecf21a64055e02541
Srujan B <66896880+srujan-b@users.noreply.github.com>
2025-01-14 14:05:14 +0530

    Add files via upload

---
+++ yolo_inference.py
@@ -0,0 +1,10 @@
+from ultralytics import YOLO
+model = YOLO("yolov8x.pt") 
+
+result  = model.predict("./input/08fd33_4.mp4", save = True)
+
+print(result[0])
+print("=====================")
+
+for box in result[0].boxes:
+    print(box)
